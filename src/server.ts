dcfff6c3bd6a4249c5b2e57a32536737c46fa9fb
farial-robama <farialrobama15@gmail.com>
2026-08-05 13:03:29 +0600

    refactor(server): add HTTP server bootstrap and graceful shutdown handlers

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,51 @@
+import http, { type Server } from 'http';
 import app from './app.js';
 import config from './config/env.js';
 
-app.listen(config.port, () => {
-  console.log(`Server running on http://localhost:${config.port}`);
-});
+const port = config.port;
+
+let server: Server;
 
 
+const gracefulShutdown = (signal: string, exitCode = 0) => {
+  return (error?: unknown) => {
+    if (error) {
+      console.error(`${signal}`, error);
+    } else {
+      console.log(`${signal} gracefully shutdown!`);
+    }
 
+    server.close(() => {
+      process.exit(exitCode);
+    });
+  };
+};
 
+const bootstrap = async () => {
+  try {
+    const httpServer = http.createServer(app);
+
+    server = httpServer.listen(port, () => {
+      console.log(`server is running on http://localhost:${port}`);
+    });
+
+    // graceful shutdown signals
+    process.on('SIGTERM', gracefulShutdown('SIGTERM'));
+    process.on('SIGINT', gracefulShutdown('SIGINT'));
+
+    // unexpected errors
+    process.on(
+      'uncaughtException',
+      gracefulShutdown('Uncaught Exception', 1),
+    );
+
+    process.on(
+      'unhandledRejection',
+      gracefulShutdown('Unhandled Rejection', 1),
+    );
+  } catch (error) {
+    console.error('Server stopped', error);
+  }
+};
+
+bootstrap();
